b032bdb9e090188d6edb3677826cc5828b63fe6e
Calculathor.sh <calculathor.sh@outlook.com>
2024-05-14 21:37:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // İleri JavaScript özellikleri buraya eklenebilir.
+function disableselect(e){
+	return false
+}
+
+function reEnable(){
+	return true
+}
+
+//if IE4+
+document.onselectstart=new Function ("return false")
+ 
+//if NS6
+if (window.sidebar){
+	document.onmousedown=disableselect
+	document.onclick=reEnable
+}
 // Şu anlık boş bırakıyorum.
